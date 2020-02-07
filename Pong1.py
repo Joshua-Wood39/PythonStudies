e1b09920bab31f13464f136d898d9a8e8af5bf01
Joshua Wood <JerichoWall@apu.edu>
2020-02-07 15:55:58 -0800

    Paddles moving

--- Pong1.py
+++ Pong1.py
@@ -29,6 +29,46 @@ paddle_b.penup()
 paddle_b.goto(350, 0)
 
 # Ball
+ball = turtle.Turtle()
+ball.speed(0)
+ball.shape("square")  # default 20px x 20px
+ball.color("red")
+ball.penup()
+ball.goto(0, 0)
+
+# Function
+
+
+def paddle_a_up():
+    y = paddle_a.ycor()
+    y += 20
+    paddle_a.sety(y)
+
+
+def paddle_a_down():
+    y = paddle_a.ycor()
+    y -= 20
+    paddle_a.sety(y)
+
+
+def paddle_b_up():
+    y = paddle_b.ycor()
+    y += 20
+    paddle_b.sety(y)
+
+
+def paddle_b_down():
+    y = paddle_b.ycor()
+    y -= 20
+    paddle_b.sety(y)
+
+
+# Keyboard binding
+wn.listen()
+wn.onkeypress(paddle_a_up, "w")
+wn.onkeypress(paddle_a_down, "s")
+wn.onkeypress(paddle_b_up, "Up")
+wn.onkeypress(paddle_b_down, "Down")
 
 
 # Main game loop
