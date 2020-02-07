203d50f157b0cdf4cb65870329bbcc9cb5a8b0ac
Joshua Wood <JerichoWall@apu.edu>
2020-02-07 15:43:47 -0800

    Beginnings of Pong

---
+++ Pong1.py
@@ -0,0 +1,36 @@
+# Simple Pong in Python 3
+# Purely functional, no classes
+
+import turtle
+
+wn = turtle.Screen()
+wn.title("Pong by Joshua-Wood39")
+wn.bgcolor("black")
+wn.setup(width=800, height=600)
+wn.tracer(0)
+
+# Paddle A
+paddle_a = turtle.Turtle()
+paddle_a.speed(0)
+paddle_a.shape("square")  # default 20px x 20px
+paddle_a.color("blue")
+paddle_a.shapesize(stretch_wid=5, stretch_len=1)
+paddle_a.penup()
+paddle_a.goto(-350, 0)
+
+
+# Paddle B
+paddle_b = turtle.Turtle()
+paddle_b.speed(0)
+paddle_b.shape("square")  # default 20px x 20px
+paddle_b.color("blue")
+paddle_b.shapesize(stretch_wid=5, stretch_len=1)
+paddle_b.penup()
+paddle_b.goto(350, 0)
+
+# Ball
+
+
+# Main game loop
+while True:
+    wn.update()
